7af34170c5184c24f5d0ea970c4ed4176f2e667d
Nguyen Xuan Anh <SimonNgj@users.noreply.github.com>
2018-05-31 10:39:44 +1000

    Update classify.py

--- classify.py
+++ classify.py
@@ -1,3 +1,5 @@
+############################################
+# Loading required libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,16 +8,20 @@ from scipy import stats
 
 plt.style.use('ggplot')
 
+#### Defining some helper functions
+# Reading function
 def read_data(file_path):
     column_names = ['user-id', 'activity', 'timestamp', 'x-axis', 'y-axis', 'z-axis']
     data = pd.read_csv(file_path, header=None, names=column_names)
     return data
 
+# Normalising function
 def feature_normalize(dataset):
     mu = np.mean(dataset, axis=0)
     sigma = np.std(dataset, axis=0)
     return (dataset - mu)/sigma
 
+# Plotting function
 def plot_axis(ax, x, y, title):
     ax.plot(x,y)
     ax.set_title(title)
@@ -23,7 +29,8 @@ def plot_axis(ax, x, y, title):
     ax.set_ylim([min(y) - np.std(y), max(y) + np.std(y)])
     ax.set_xlim([min(x), max(x)])
     ax.grid(True)
-
+				
+# Plotting function
 def plot_activity(activity, data):
     fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize = (15, 10), sharex = True)
     plot_axis(ax0, data['timestamp'], data['x-axis'], 'x-axis')
@@ -34,14 +41,17 @@ def plot_activity(activity, data):
     plt.subplots_adjust(top=0.90)
     plt.show()
 
+# Read the data set and normalise each of the accelerometer component
 dataset = read_data('actitracker_modified2.txt')
+dataset.dropna(axis=0, how='any', inplace= True)
 dataset['x-axis'] = feature_normalize(dataset['x-axis'])
 dataset['y-axis'] = feature_normalize(dataset['y-axis'])
 dataset['z-axis'] = feature_normalize(dataset['z-axis'])
 
-# for activity in np.unique(dataset["activity"]):
-#     subset = dataset[dataset["activity"] == activity][:180]
-#     plot_activity(activity, subset)
+# Visualize each component of accelerometer for different activities
+for activity in np.unique(dataset["activity"]):
+	subset = dataset[dataset["activity"] == activity][:180]
+	plot_activity(activity, subset)
 
 def windows(data, size):
     start = 0
@@ -61,20 +71,23 @@ def segment_signal(data, window_size=90):
         if (len(dataset['timestamp'][start:end]) == window_size):
             segments = np.vstack([segments, np.dstack([x,y,z])])
             labels = np.append(labels, stats.mode(data["activity"][start:end])[0][0])
-            print("segment_sig_count: ", it_count)
+            #print("segment_sig_count: ", it_count)
     return segments, labels
 
 segments, labels = segment_signal(dataset)
 labels = np.asarray(pd.get_dummies(labels), dtype=np.int8)
 reshaped_segments = segments.reshape(len(segments), 1, 90, 3)
 
+# Divide data into training and testing set (70/30) randomly
 train_test_split = np.random.rand(len(reshaped_segments)) < 0.70
 train_x = reshaped_segments[train_test_split]
 train_y = labels[train_test_split]
 test_x = reshaped_segments[~train_test_split]
 test_y = labels[~train_test_split]
 
+##########################################
 #CNN model
+# Define some helper functions and configuration variable for our CNN model
 
 input_height = 1
 input_width = 90
@@ -87,7 +100,7 @@ depth = 60
 num_hidden = 1000
 
 learning_rate = 0.0001
-training_epochs = 5
+training_epochs = 20
 
 total_batches = reshaped_segments.shape[0] // batch_size
 
@@ -111,6 +124,7 @@ def apply_max_pool(x, kernel_size, stride_size):
     return tf.nn.max_pool(x, ksize=[1, 1, kernel_size, 1],
                           strides=[1, 1, stride_size, 1], padding='VALID')
 
+# Tensorflow placeholders for input and output data				
 X = tf.placeholder(tf.float32, shape=[None, input_height, input_width, num_channels])
 Y = tf.placeholder(tf.float32, shape=[None, num_labels])
 
@@ -121,14 +135,17 @@ c = apply_depthwise_conv(p, 6, depth*num_channels, depth//10)
 shape = c.get_shape().as_list()
 c_flat = tf.reshape(c, [-1, shape[1]*shape[2]*shape[3]])
 
+# The tanh function is used as non-linearity
 f_weights_l1 = weight_variable([shape[1]*shape[2]*depth*num_channels*(depth//10), num_hidden])
 f_biases_l1 = bias_variable([num_hidden])
 f = tf.nn.tanh(tf.add(tf.matmul(c_flat, f_weights_l1),f_biases_l1))
 
+# the Softmax layer is defined to output probabilities of the class labels
 out_weights = weight_variable([num_hidden, num_labels])
 out_biases = bias_variable([num_labels])
 y_ = tf.nn.softmax(tf.matmul(f, out_weights) + out_biases)
 
+# The negative log-likelihood cost function will be minimised using stochastic gradient descent optimizer
 loss = -tf.reduce_sum(Y*tf.log(y_))
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
 
@@ -137,6 +154,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 cost_history = np.empty(shape=[1], dtype=float)
 
+# Training the model
 with tf.Session() as sess:
     tf.global_variables_initializer().run()
     for epoch in range(training_epochs):
